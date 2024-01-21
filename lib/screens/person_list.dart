fcda75f8d0d588681f83f71a888cfeb7f7305f59
palash <palashbhasme123@gmail.com>
2024-01-21 15:42:03 +0530

    Initial commit

---
+++ lib/screens/person_list.dart
@@ -0,0 +1,15 @@
+import "package:flutter/material.dart";
+
+class PersonList extends StatefulWidget {
+  const PersonList({super.key});
+
+  @override
+  State<PersonList> createState() => _PersonListState();
+}
+
+class _PersonListState extends State<PersonList> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
